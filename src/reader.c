9d50a4ead9c94994b8d56221228bf40ed44e9407
NateChoe1 <thetinybros@gmail.com>
2021-02-02 15:10:56 -0600

    Added a way to scroll down with J

--- src/reader.c
+++ src/reader.c
@@ -22,6 +22,8 @@
 #include <curses.h>
 #include <stdio.h>
 
+#define HIGHLIGHTED_TEXT 1
+
 unsigned long long searchString(FILE *file, char searchText[]) {
 	int stringLength = strlen(searchText);
 	char buffer[stringLength];
@@ -39,7 +41,7 @@ unsigned long long searchString(FILE *file, char searchText[]) {
 
 void createLookup(FILE *data, FILE *lookup) {
 	unsigned long long pageCount = 0;
-	while (1) {
+	for (;;) {
 		unsigned long long currentPosition = searchString(data, "<page>");
 		if (currentPosition == -1)
 			break;
@@ -55,8 +57,8 @@ void createLookup(FILE *data, FILE *lookup) {
 
 		for (int i = 0; i < sizeof(currentPosition); i++)
 			fputc(positionArray[i], lookup);
-		searchString(data, "<title>");
 		fputc('\0', lookup);
+		searchString(data, "<title>");
 		char c = fgetc(data);
 		while (c != '<') {
 			fputc(c, lookup);
@@ -71,26 +73,44 @@ void createLookup(FILE *data, FILE *lookup) {
 	fclose(lookup);
 }
 
-void displayScreen(unsigned long long lookupPosition, unsigned long long cursorPosition, FILE *lookup) {
-	fseek(lookup, sizeof(unsigned long long) + 1, SEEK_SET);
-	//            [8 bytes of lookup position][the name of the article]\0
-	//            ^- lookupPosition           ^- what we want
+void displayScreen(unsigned long long lookupPosition, unsigned long long lookupIndex, unsigned long long scrollingIndex, FILE *lookup) {
+	fseek(lookup, lookupPosition, SEEK_SET);
+	//            [8 bytes of lookup position]\0[the name of the article]\0
+	//            ^- lookupPosition             ^- what we want
+
 	for (int i = 0; i < LINES; i++) {
 		for (int j = 0; j < COLS; j++) {
 			char c = fgetc(lookup);
 			if (c == 0) {
 				fseek(lookup, sizeof(unsigned long long) + 1, SEEK_CUR);
+				scrollingIndex++;
 				break;
 			}
+			if (lookupIndex == scrollingIndex)
+				attron(COLOR_PAIR(HIGHLIGHTED_TEXT));
+			else
+				attroff(COLOR_PAIR(HIGHLIGHTED_TEXT));
 			mvaddch(i, j, c);
 		}
 	}
 }
 
+void scrollDown(unsigned long long *lookupIndex, unsigned long long *scrollingIndex, unsigned long long *lookupPosition, FILE *lookup) {
+	(*scrollingIndex)++;
+	(*lookupPosition)++;
+	fseek(lookup, *lookupPosition, SEEK_SET);
+	char c = 1;
+	while (c != 0) {
+		*lookupPosition = *lookupPosition + 1;
+		c = fgetc(lookup);
+	}
+	(*lookupPosition) += 9;
+}
+
 int main(int argc, char *argv[]) {
 	FILE *data = NULL;
 	FILE *lookup = NULL;
-	int lookupIndex;
+	int lookupArgumentIndex;
 	for (int i = 0; i < argc; i++) {
 		if (strcmp(argv[i], "--help") == 0 | strcmp(argv[i], "-h") == 0) {
 			printf(
@@ -110,26 +130,51 @@ int main(int argc, char *argv[]) {
 		if (strcmp(argv[i], "-d") == 0)
 			data = fopen(argv[i+1], "r");
 		if (strcmp(argv[i], "-l") == 0)
-			lookupIndex = i;
+			lookupArgumentIndex = i;
 	}
 	//Parse command line arguments
 
-	if (access(argv[lookupIndex+1], F_OK) != 0) {
-		lookup = fopen(argv[lookupIndex+1], "w");
+	if (access(argv[lookupArgumentIndex+1], F_OK) != 0) {
+		lookup = fopen(argv[lookupArgumentIndex+1], "w");
 		createLookup(data, lookup);
 	}
 	//At this point, arguments have been parsed and a lookup file exists.
 
-	lookup = fopen(argv[lookupIndex+1], "r");
-	unsigned long long lookupPosition = 0;
+	lookup = fopen(argv[lookupArgumentIndex+1], "r");
 	initscr();
 	cbreak();
 	noecho();
-	displayScreen(0, 0, lookup);
-	refresh();
-	while (getch() != '`') {
-		displayScreen(0, 0, lookup);
-		refresh();
+	curs_set(0);
+	start_color();
+	init_pair(HIGHLIGHTED_TEXT, COLOR_BLACK, COLOR_WHITE);
+	unsigned long long lookupIndex = 0;
+	unsigned long long scrollingIndex = 0;
+	unsigned long long lookupPosition = 9;
+	for (;;) {
+		clear();
+		displayScreen(lookupPosition, lookupIndex, scrollingIndex, lookup);
+		char shouldEnd = 0;
+		switch (getch()) {
+			case '`':
+				shouldEnd = 1;
+				break;
+			case 'j':
+				lookupIndex++;
+				break;
+			case 'k':
+				lookupIndex -= 1;
+				break;
+			case 'J':
+				scrollDown(&lookupIndex, &scrollingIndex, &lookupPosition, lookup);
+				break;
+			case 'K':
+				break;
+			default:
+				break;
+
+		}
+		if (shouldEnd)
+			break;
 	}
 	endwin();
 
